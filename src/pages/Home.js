7231478d46af40ed5d72c50150a73ed16255d921
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 15:28:22 +0430

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,10 @@
 import { Link, Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <>
-      <Navbar />
-      <section className='section'>
-        <Outlet />
+    <section className='section'>
+      <h2>home page</h2>
       </section>
-    </>
   );
 };
 export default Home;
