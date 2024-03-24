5ce9913227bfadd8f332f34de940f15c7bd3b7b9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 12:54:16 +0430

    imports home, about and products

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>home page</div>} />
-        <Route path="/testing" element={<div>
-          <h2>HELLO ROUTER</h2>
-        </div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   )
