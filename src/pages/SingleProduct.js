24b98c0327b832e18b4231c0e5cc31801d603202
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 19:54:31 +0430

    display product

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,11 +1,16 @@
 import { Link, useParams } from "react-router-dom";
+import products from '../data'
 
 const SingleProduct = () => {
   const { productId } = useParams()
+  const product = products.find((product) => product.id === productId)
+
+  const { name, image } = product
   return (
     <section className='section product'>
-      <h2>{productId}</h2>
-      <Link to='/products'>Bacl to products</Link>
+      <img src={image} alt={name} />
+      <h5>{name}</h5>
+      <Link to='/products'>back to products</Link>
     </section>
   );
 };
