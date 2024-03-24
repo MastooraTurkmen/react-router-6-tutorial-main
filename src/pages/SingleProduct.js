6b37744e64f3c646c5a91a5fc15cf018858c8ca1
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 15:28:26 +0430

    single product

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,11 @@
+import { Link, useParams } from "react-router-dom";
+
 const SingleProduct = () => {
+  const { productId } = useParams()
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h2>{productId}</h2>
+      <Link to='/products'>Bacl to products</Link>
     </section>
   );
 };
