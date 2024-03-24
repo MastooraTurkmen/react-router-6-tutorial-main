cef91ac7fe38cd9ea4ab36358b67e84df474a3a9
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 13:19:54 +0430

    adds products

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
