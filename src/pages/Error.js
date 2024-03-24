3cc07c07152d2b98fdf3517f4dea684928a94103
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 13:19:16 +0430

    set up Error

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <section className='section'>
       <h2>Error</h2>
+      <Link to='/'>Back to home</Link>
     </section>
   );
 };
