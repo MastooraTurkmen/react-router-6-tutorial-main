0df53e3fbd8b4abb6fccb0d5d9644253a33bf56d
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 12:39:10 +0430

    install react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>home page</div>} />
+        <Route path="/testing" element={<div>
+          <h2>HELLO ROUTER</h2>
+        </div>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
