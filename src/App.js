fbbb474c4504080e6990091460df614b674b0313
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 15:27:59 +0430

    single product

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import Error from './pages/Error'
+import SharedLink from './components/SharedLink'
+import SingleProduct from './pages/SingleProduct'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} >
+        <Route path="/" element={<SharedLink />} >
+          <Route index path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
+          <Route path="products/:productId" element={<SingleProduct />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
