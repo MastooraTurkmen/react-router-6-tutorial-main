afa8aad25faae9d78a60c2492cb06aa38e25e09b
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 19:54:28 +0430

    display products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom';
+import products from '../data'
+
 const Products = () => {
   return (
     <>
       <section className='section'>
         <h2>products</h2>
+        <div className="products">
+          {products.map(({ id, name }) => {
+            return (
+              <article key={id}>
+                <h5>{name}</h5>
+                <Link to={`/products/${id}`}>more info</Link>
+              </article>
+            )
+          })}
+        </div>
       </section>
     </>
   );
