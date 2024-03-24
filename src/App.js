64ba6f950fa667b74186aaf72b80d373c4fb8958
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-24 13:19:14 +0430

    set up Error

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
+import Error from './pages/Error'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="products" element={<Products />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   )
