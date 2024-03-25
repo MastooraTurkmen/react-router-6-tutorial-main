b3483d8f7db858141d64eb90b2dfe7bf26292bea
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 19:54:34 +0430

    set up react router

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,23 +1,20 @@
 import { Link } from 'react-router-dom';
-import products from '../data'
+import products from '../data';
 
 const Products = () => {
   return (
-    <>
-      <section className='section'>
-        <h2>products</h2>
-        <div className="products">
-          {products.map(({ id, name }) => {
-            return (
-              <article key={id}>
-                <h5>{name}</h5>
-                <Link to={`/products/${id}`}>more info</Link>
-              </article>
-            )
-          })}
-        </div>
-      </section>
-    </>
+    <section className='section'>
+      <div className='products'>
+        {products.map((product) => {
+          return (
+            <article key={product.id}>
+              <h5>{product.name}</h5>
+              <Link to={`/products/${product.id}`}>more info</Link>
+            </article>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
