b6a3a81b4b759b03bc3cc172cd445ab8faf990bb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 19:29:20 +0430

    set up Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,17 @@
 import { useState } from 'react';
-const Login = () => {
+import { useNavigate } from 'react-router-dom';
+
+const Login = ({ setUser }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!name || !email) return;
+    setUser({ name: name, email: email })
+    navigate('/dashboard')
   };
 
   return (
