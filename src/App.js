bfa31317a050e0ff9c890da6edba861d03ec786b
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 19:29:26 +0430

    set up app js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import Products from './pages/Products'
 import Error from './pages/Error'
 import SharedLink from './components/SharedLink'
 import SingleProduct from './pages/SingleProduct'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SharedLink />} >
           <Route index path="/" element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard user={user}></Dashboard>} />
+          <Route path="login" element={<Login setUser={setUser}></Login>} />
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
           <Route path="*" element={<Error />} />
