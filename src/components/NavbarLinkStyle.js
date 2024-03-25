087871afa8080b1bb6baae88b39b304135b4bdab
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 19:28:56 +0430

    Navbar set up

--- src/components/NavbarLinkStyle.js
+++ src/components/NavbarLinkStyle.js
@@ -6,6 +6,8 @@ const NavbarLinkStyle = () => {
             <NavLink className={({ isActive }) => (isActive ? 'link active' : 'link')} to='/'>Home</NavLink>
             <NavLink className={({ isActive }) => (isActive ? 'link active' : 'link')} to='/about'>About</NavLink>
             <NavLink className={({ isActive }) => (isActive ? 'link active' : 'link')} to='/products'>Products</NavLink>
+            <NavLink className={({ isActive }) => (isActive ? 'link active' : 'link')} to='/dashboard'>Dashboard</NavLink>
+            <NavLink className={({ isActive }) => (isActive ? 'link active' : 'link')} to='/login'>Login</NavLink>
         </nav>
     );
 }
