652b6edc943e81ac7ab9dd195b125ca0d66e0a89
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-25 19:53:44 +0430

    set up react router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+import Error from './pages/Error';
+import SharedLayout from './pages/SharedLayout';
+import SingleProduct from './pages/SingleProduct';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import ProtectedRoute from './pages/ProtectedRoute';
+import SharedProductLayout from './pages/SharedProductLayout';
 
-import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import Error from './pages/Error'
-import SharedLink from './components/SharedLink'
-import SingleProduct from './pages/SingleProduct'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState(null)
-
+  const [user, setUser] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SharedLink />} >
-          <Route index path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard user={user}></Dashboard>} />
-          <Route path="login" element={<Login setUser={setUser}></Login>} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:productId" element={<SingleProduct />} />
-          <Route path="*" element={<Error />} />
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+
+          <Route path='products' element={<SharedProductLayout />}>
+            <Route index element={<Products />} />
+            <Route path=':productId' element={<SingleProduct />} />
+          </Route>
+
+          <Route path='login' element={<Login setUser={setUser}></Login>} />
+          <Route
+            path='dashboard'
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user} />
+              </ProtectedRoute>
+            }
+          />
+          <Route path='*' element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
